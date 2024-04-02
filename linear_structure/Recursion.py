906ea2049fdda0417f15b21da4409c8a11235619
fish_super <114349415+fishsuper@users.noreply.github.com>
2024-04-02 23:22:23 +0800

    Add files via upload

--- linear_structure/Recursion.py
+++ linear_structure/Recursion.py
@@ -62,3 +62,47 @@ t.down()
 t.color('green')
 tree(110,t)
 myWin.exitonclick()
+
+# 汉诺塔问题
+def moveTower(height:int, fromPole:str, toPole:str, withPole:str):
+    '''
+    height是正整数, 其他3个参数是字符串类型
+    '''
+    if height>=1:
+        moveTower(height-1, fromPole, withPole, toPole)
+        moveDisk(fromPole, toPole)
+        moveTower(height-1, withPole, toPole, fromPole)
+
+def moveDisk(fp,tp):
+    print('moving disk from', fp, 'to', tp)
+    
+print('-----------汉诺塔方案: 5个盘子------------')
+moveTower(5, 'the fist pole', 'the third pole', 'the second pole')
+
+
+# 动态规划算法解决找零问题
+def dpMakeChange(coinValueList, change, minCoins, coinsUsed):
+    for cents in range(change+1):
+        coinCount = cents  # 初始化为全用1分的硬币
+        newCoin = 1   # 新使用的硬币初始化为1
+        for j in [c for c in coinValueList if c<=cents]:
+            if minCoins[cents-j]+1 < coinCount:
+                coinCount = minCoins[cents-j]+1
+                newCoin = j
+            minCoins[cents] = coinCount
+            coinsUsed[cents] = newCoin
+        return minCoins[change]
+    
+def printCoins(coinsUsed, change):
+    coin = change
+    while coin>0:
+        thisCoin = coinsUsed[coin]
+        print(thisCoin)
+        coin = coin-thisCoin
+
+cl = [1,5,10,21,25]
+coinsUsed = [0]*64
+coinCount = [0]*64
+dpMakeChange(cl, 63, coinCount, coinsUsed)
+print(coinsUsed)        
+printCoins(coinsUsed, 63)
